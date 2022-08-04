d1d806486e1b5b803e82d680607e320f5c05811f
RyotaKITA-12 <s2022007@stu.musashino-u.ac.jp>
2022-08-04 16:03:25 +0900

    add api-login

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -7,6 +7,7 @@ import (
 
 func Setup(app *fiber.App) {
 	app.Get("/api/user", controllers.User)
+	app.Get("/api/logout", controllers.Logout)
 	app.Post("/api/register", controllers.Register)
 	app.Post("/api/login", controllers.Login)
 }
