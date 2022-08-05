2228c5d9e13980dbbf6892bbc03ab2d6faf0d634
RyotaKITA-12 <s2022007@stu.musashino-u.ac.jp>
2022-08-05 19:30:29 +0900

    add map-marker

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,25 +1,39 @@
 <template>
-    <GMapMap :center="{ lat: 51.093048, lng: 6.842120 }" :zoom="7" map-type-id="terrain"
-        style="width: 100vw; height: 900px">
+    <GMapMap :center="center" :zoom="10" map-type-id="terrain" style="width: 100vw; height: 100vh">
         <GMapMarker :key="marker.id" v-for="marker in markers" :position="marker.position" />
     </GMapMap>
 </template>
 
 <script >
+import { ref, onMounted } from 'vue'
 import axios from 'axios'
+import { useStore } from "vuex"
+
 
 export default {
     name: 'App',
     setup() {
-        const center = { lat: 51.093048, lng: 6.842120 }
-        const markers = [
-            {
-                id: 'dfsldjl3r',
-                position: {
-                    lat: 51.093048, lng: 6.842120
-                },
+        const center = { lng: 139.7671248, lat: 35.6812362 }
+        const store = useStore()
+        const markers = ref([])
+        onMounted(async () => {
+            try {
+                const { data } = await axios.get('post')
+                data.forEach(elem => {
+                    var e = ref({})
+                    e = {
+                        id: elem.ID,
+                        position: {
+                            lat: elem.latitude, lng: elem.longitude
+                        },
+                    }
+                    markers.value.push(e)
+                })
+                await store.dispatch('setAuth', true)
+            } catch (e) {
+                await store.dispatch('setAuth', false)
             }
-        ]
+        })
 
         return {
             center,
