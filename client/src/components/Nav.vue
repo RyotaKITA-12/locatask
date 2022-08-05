aab7b3ecfda88274073c39f43ef2171aeef103f8
RyotaKITA-12 <s2022007@stu.musashino-u.ac.jp>
2022-08-05 15:27:15 +0900

    add post-button

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -10,6 +10,11 @@
             </ul>
             <ul class="navbar-nav my-2 my-lg-0 ms-auto">
                 <li class="nav-item">
+                    <div class="nav-link">
+                        <img @click="openModal" src="./img/pen.png" height="40" border="2" style="border-radius: 0.5em">
+                    </div>
+                </li>
+                <li class="nav-item">
                     <router-link to="/login" class="nav-link" @click="logout">
                         <button class="btn btn-outline-danger">ログアウト</button>
                     </router-link>
@@ -26,6 +31,11 @@
             </ul>
             <ul class="navbar-nav my-2 my-lg-0 ms-auto">
                 <li class="nav-item">
+                    <div class="nav-link">
+                        <img @click="openModal" src="./img/pen.png" height="40" border="2" style="border-radius: 0.5em">
+                    </div>
+                </li>
+                <li class="nav-item">
                     <router-link to="/login" class="nav-link">
                         <button class="btn btn-outline-primary">ログイン</button>
                     </router-link>
@@ -36,12 +46,49 @@
                     </router-link>
                 </li>
             </ul>
+            <br>
+            <div class="text-center">
+                <div id="overlay" v-show="showContent">
+                    <form @submit.prevent="submit">
+                        <div id="content" class="rounded">
+                            <button class="btn-circle btn-outline-dark " @click="closeModal">x</button>
+                            <h1 class="h1 mb-3 fw-normal text-success"><b>TASK</b></h1>
+                            <hr>
+                            <br>
+                            <div class="input-container">
+                                <label for="title" class="text-secondary"><b>タイトル</b></label>
+                                <input id="title" v-model="title" class="form-control" placeholder="ミーティング" required>
+                            </div>
+                            <br>
+                            <br>
+                            <div class="input-container">
+                                <label for="period" class="text-secondary"><b>期限</b></label>
+                                <input id="period" type="date" v-model="period"
+                                    :min="new Date().toISOString().split('T')[0]">
+                            </div>
+                            <br>
+                            <br>
+                            <div class="input-container">
+                                <label for="locate" class="text-secondary"><b>場所</b></label>
+                                <input id="locate" v-model="locate" class="form-control" placeholder="東京駅" required>
+                            </div>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <button class="w-50 btn btn-lg btn-success" type="submit">登録する</button>
+                            <br>
+                            <br>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </template>
     </nav>
 </template>
 
 <script>
-import { computed } from 'vue'
+import { ref, computed } from 'vue'
 import { useStore } from 'vuex'
 import { useRouter } from 'vue-router'
 import axios from 'axios'
@@ -57,7 +104,122 @@ export default {
             store.dispatch('setAuth', false)
             await router.push('/login')
         }
-        return { auth, logout }
+        var showContent = ref(false)
+        const openModal = () => {
+            showContent.value = true
+        }
+        const closeModal = () => {
+            showContent.value = false
+        }
+
+        return {
+            auth,
+            logout,
+            openModal,
+            showContent,
+            closeModal
+        }
     }
 }
 </script>
+
+<style>
+.form-profile {
+    width: 100%;
+    max-width: 500px;
+    padding: 15px;
+    margin: auto;
+}
+
+.form-profile .form-control {
+    position: relative;
+    box-sizing: border-box;
+    height: auto;
+    padding: 10px;
+    font-size: 16px;
+}
+
+.form-profile .form-control:focus {
+    z-index: 2;
+}
+
+/* .profile-container div {       */
+/*     background-color: #F0F0F5; */
+/* }                              */
+
+.container p {
+    display: inline;
+    text-align: left;
+}
+
+.profile-container h2 {
+    text-align: left;
+}
+
+.profile-rect svg {
+    margin-left: 5px;
+    margin-right: 5px;
+}
+
+.rect-left {
+    float: left;
+}
+
+.bg-gray {
+    background-color: #F5F5FF;
+}
+
+#overlay {
+    z-index: 1;
+
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+}
+
+#content {
+    z-index: 2;
+    width: 450px;
+    padding: 1em;
+    background: #fff;
+}
+
+input[type="date"] {
+    padding: 0 10px;
+    width: 200px;
+    height: 40px;
+    border: solid 1px #CCC;
+    box-sizing: border-box;
+    border-radius: 5px;
+    font-size: 15px;
+    color: #999;
+    box-shadow: 0px;
+}
+
+.btn-circle {
+    font-size: 100%;
+    font-weight: bold;
+    border: 1px solid #999;
+    color: #999;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100%;
+    width: 1.5em;
+    line-height: 1.3em;
+    cursor: pointer;
+    transition: .2s;
+}
+
+.shadow-bg {
+    box-shadow: 0 10px 25px 0 rgba(80, 134, 135, 0.3);
+}
+</style>
--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,20 +1,30 @@
 <template>
-  <GMapMap
-      :center="{lat: 51.093048, lng: 6.842120}"
-      :zoom="7"
-      map-type-id="terrain"
-      style="width: 100vw; height: 900px"
-  >
-  </GMapMap>
+    <GMapMap :center="{ lat: 51.093048, lng: 6.842120 }" :zoom="7" map-type-id="terrain"
+        style="width: 100vw; height: 900px">
+        <GMapMarker :key="marker.id" v-for="marker in markers" :position="marker.position" />
+    </GMapMap>
 </template>
 
 <script >
+import axios from 'axios'
+
 export default {
-  name: 'App',
-  data() {
-    return {
-      center: {lat: 51.093048, lng: 6.842120},
+    name: 'App',
+    setup() {
+        const center = { lat: 51.093048, lng: 6.842120 }
+        const markers = [
+            {
+                id: 'dfsldjl3r',
+                position: {
+                    lat: 51.093048, lng: 6.842120
+                },
+            }
+        ]
+
+        return {
+            center,
+            markers,
+        }
     }
-  }
 }
 </script>
